b05881276171fd58b865c2e6963cfde525eae37e
Luc Libralesso <libralesso.l@gmail.com>
2016-05-28 15:15:46 -0400

    initial commit

---
+++ game-controls.js
@@ -0,0 +1,80 @@
+
+function KeyboardControls(object, options) {
+  this.object = object;
+  options = options || {};
+  this.domElement = options.domElement || document;
+  this.moveSpeed = options.moveSpeed || 1;
+  this.zoomCounter = 0;
+  this.domElement.addEventListener('keydown', this.onKeyDown.bind(this), false);
+  this.domElement.addEventListener('keyup', this.onKeyUp.bind(this), false);
+  this.domElement.addEventListener('mousewheel', this.onMouseWheel.bind(this), false);
+  this.domElement.addEventListener('DOMMouseScroll', this.onMouseWheel.bind(this), false);
+}
+
+KeyboardControls.prototype = {
+  update: function() {
+    if ( this.moveForward && this.object.position.z > 0 )
+      this.object.position.z -= this.moveSpeed;
+    if ( this.moveBackward && this.object.position.z < 100 )
+      this.object.position.z += this.moveSpeed;
+    if ( this.moveLeft && this.object.position.x > -100 )
+      this.object.position.x -= this.moveSpeed;
+    if ( this.moveRight && this.object.position.x < 100 )
+      this.object.position.x += this.moveSpeed;
+  },
+
+  onMouseWheel: function(event) {
+    if ( event.wheelDeltaY ) {
+      var direction = -2*event.wheelDeltaY/Math.abs(event.wheelDeltaY);
+    } else { // firefox
+      var direction = 2*event.detail;
+    }
+    if ( this.zoomCounter > -100 || direction > 0 ) {
+      if ( this.zoomCounter <= 10 || direction < 0 ) {
+        this.zoomCounter += direction;
+        this.object.translateZ(direction);
+        this.object.rotation.x += -0.005*direction;
+      }
+    }
+  },
+
+  onKeyDown: function(event) {
+    switch (event.keyCode) {
+      case 38: /* up */
+      case 87: /* W */
+        this.moveForward = true;
+        break;
+      case 37:
+      case 65: /*A*/
+        this.moveLeft = true;
+        break;
+      case 40: /*down */
+      case 83:
+        this.moveBackward = true; break;
+      case 39:
+      case 68: /* right */
+        this.moveRight = true; break;
+    }
+  },
+
+  onKeyUp: function(event) {
+    switch (event.keyCode) {
+      case 38: /* up */
+      case 87: /* W */
+        this.moveForward = false;
+        break;
+      case 37:
+      case 65: /*A*/
+        this.moveLeft = false;
+        break;
+      case 40: /*down */
+      case 83:
+        this.moveBackward = false;
+        break;
+      case 39:
+      case 68: /* right */
+        this.moveRight = false;
+        break;
+    }
+  }
+}
