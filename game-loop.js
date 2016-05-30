b37d1773ab62d3e2648d4df817bd126c0a5968e3
Luc Libralesso <libralesso.l@gmail.com>
2016-05-30 09:47:51 -0400

    add grayscale on cities tooltips that are not available

--- game-loop.js
+++ game-loop.js
@@ -50,8 +50,13 @@ function updateTooltips()
 function tooltipContent(i) {
   var tooltip = world.cities[i].tooltip;
   var text = '';
-  if (world.cities[i].player == 'green') text += '<ul class="back-green">'
-  else text += '<ul class="back-red">'
+  if (world.cities[i].player == 'green') {
+    if ( world.cities[i].available ) {
+      text += '<ul class="back-green">'
+    } else {
+      text += '<ul class="back-blocked">'
+    }
+  } else text += '<ul class="back-red">'
   text += '<li><img src="gold.png"/><span class="value">'+world.cities[i].gold+'</span></li>';
   text += '<li><img src="sword.png"/><span class="value">'+world.cities[i].soldiers+'</span></li>';
   text += '<li><img src="shield.png"/><span class="value">'+world.cities[i].defense+'</span></li>';
