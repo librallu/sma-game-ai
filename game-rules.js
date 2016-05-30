d228ea2cf725005b34ae2435c65301e5f6a883ea
Luc Libralesso <libralesso.l@gmail.com>
2016-05-30 09:35:42 -0400

    add action limit in turn

--- game-rules.js
+++ game-rules.js
@@ -18,26 +18,40 @@ function reactUserClick() {
 }
 
 
-function SendSoldiers(src, dest) {
-  if ( rules_data.current_player == world.cities[src].player ) {
-    console.log("sending troops from " + src + " to " + dest);
-    if (world.cities[src].player == world.cities[dest].player) { // same player
-      world.cities[dest].soldiers += world.cities[src].soldiers;
-      world.cities[src].soldiers = 0;
-    }
-    else {
-      // soldiers vs soldiers
-      var lastSoldiersFirst = world.cities[src].soldiers - world.cities[dest].soldiers
-      world.cities[dest].soldiers = Math.max(world.cities[dest].soldiers - world.cities[src].soldiers, 0)
-      world.cities[src].soldiers = Math.max(lastSoldiersFirst, 0);
+function checkActionUsed(i) {
+  return world.cities[i].available;
+}
 
-      // soldiers vs defense
-      var lastSoldiersSecond = world.cities[src].soldiers - world.cities[dest].defense;
-      world.cities[dest].defense = Math.max(world.cities[dest].defense - world.cities[src].soldiers, 0);
-      world.cities[src].soldiers = Math.max(lastSoldiersSecond, 0);
+function actionUsed(i) {
+  world.cities[i].available = false;
+}
+
+
+function SendSoldiers(src, dest) {
+  if ( checkActionUsed(src) ) {
+    if ( rules_data.current_player == world.cities[src].player ) {
+      console.log("sending troops from " + src + " to " + dest);
+      actionUsed(src);
+      if (world.cities[src].player == world.cities[dest].player) { // same player
+        world.cities[dest].soldiers += world.cities[src].soldiers;
+        world.cities[src].soldiers = 0;
+      }
+      else {
+        // soldiers vs soldiers
+        var lastSoldiersFirst = world.cities[src].soldiers - world.cities[dest].soldiers
+        world.cities[dest].soldiers = Math.max(world.cities[dest].soldiers - world.cities[src].soldiers, 0)
+        world.cities[src].soldiers = Math.max(lastSoldiersFirst, 0);
+
+        // soldiers vs defense
+        var lastSoldiersSecond = world.cities[src].soldiers - world.cities[dest].defense;
+        world.cities[dest].defense = Math.max(world.cities[dest].defense - world.cities[src].soldiers, 0);
+        world.cities[src].soldiers = Math.max(lastSoldiersSecond, 0);
+      }
+    } else {
+      console.log('Invalid move: can\'t move enemies soldiers.');
     }
   } else {
-    console.log('Invalid move: can\'t move enemies soldiers.')
+    console.log('This city has already played this turn');
   }
 }
 
